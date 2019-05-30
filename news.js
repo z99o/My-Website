062f4a8a7bfbe81d096f9997f0c6ce2d45bbddde
Ryan Little <ryan.jay.little@gmail.com>
2019-05-30 14:19:08 -0700

    Added some comments

--- news.js
+++ news.js
@@ -1,16 +1,22 @@
+
+// Handle user's clicks
 window.addEventListener('click', function(event)
 {
+
+  // When the user clicks the post button
   if(event.target.classList.contains("accept-button"))
   {
     console.log("post button clicked");
   }
 
+  // When the user clicks the see comments button
   else if(event.target.classList.contains("view-comments-button"))
   {
     viewComments(event);
   }
 });
 
+// Shows/hides comments of the post that the user clicked on
 function viewComments(event)
 {
   var post = event.target.parentNode.parentNode;
