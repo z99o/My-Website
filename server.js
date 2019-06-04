5cdca0747beec1fbfc4fce40c1cdcb30f1ae4d1a
z99o <z99o@users.noreply.github.com>
2019-06-04 16:26:54 -0700

    Broken file

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/',function(req,res,next){
     console.log("==Server Displaying Index");
-    
+
 });
+
+app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+  });
