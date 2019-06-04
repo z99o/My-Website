910cd1e16167b2491541e7ce3957ef38e8e45381
z99o <z99o@users.noreply.github.com>
2019-06-04 16:01:02 -0700

    server changes

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var http = require('http');
-var handler = require('./handler');
+var handler = require('./server_function');
 
 
-var server = http.createServer(handler.handleRequest).listen(3000);
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+var port = process.env.PORT || 3000;
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
